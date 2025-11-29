d7f3706e8a36bf9d8a775ca933b7acfba2148618
sivavek <sivakumarvivek.devops@gmail.com>
2025-11-30 01:22:06 +0530

    update ignore

--- 03-first-tf-project/providers.tf
+++ 03-first-tf-project/providers.tf
@@ -2,11 +2,16 @@ terraform {
   required_version = ">= 1.11.0"
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "~> 5.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
     }
   }
 }
+
 provider "aws" {
   region = "ap-south-1"
 }
